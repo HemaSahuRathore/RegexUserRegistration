252020215f9d3c12d6ce7fb89ff97179f5b903a2
Hema <hemasahu019@gmail.com>
2022-08-16 13:13:09 +0530

    Added UC1 code to validate first Name

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace RegexUserRegistration
 {
     public class UserRegistration
     {
+        //Method to validate Name
         public static void ValidateName(string name)
         {
-            if (System.Text.RegularExpressions.Regex.IsMatch(name, UserRegistrationPattern.namePattern))
+            if (Regex.IsMatch(name, UserRegistrationPattern.namePattern))
                 Console.WriteLine("{0} is valid", name);            
             else
                 Console.WriteLine("Please enter valid name");       
