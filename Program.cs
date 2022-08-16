0866cec4005e0e22434979672aa4d141cd13b8f9
Hema <hemasahu019@gmail.com>
2022-08-16 13:16:24 +0530

    Added UC2 code to validate last Name

--- Program.cs
+++ Program.cs
@@ -1,24 +1,20 @@
 ﻿namespace RegexUserRegistration
 {   /// <summary>
-    /// UC 1 : As a User need to enter a valid First Name
-    /// - First name starts with Cap and hasminimum 3 characters
+    /// UC 2 : As a User need to enter a valid last Name
+    /// - Last name starts with Cap and has minimum 3 characters
     /// </summary>
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to User Registration Problem"); //welcome msg
+            Console.WriteLine("Welcome to User Registration Problem"); 
             Console.Write("\nEnter your First Name : ");
             string firstName = Console.ReadLine();
             UserRegistration.ValidateName(firstName);
-
             Console.Write("\nEnter your Last Name : ");
             string lastName = Console.ReadLine();
             UserRegistration.ValidateName(lastName);
 
-            Console.Write("Email ID : ");
-            string emailId = Console.ReadLine();
-            Console.ReadLine();
         }
     }
 }
